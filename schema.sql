c1580666746b4a8888a199dcf84038342423097a
Manuel Corral <manuelcorral@manuels-mini.lan>
2023-03-08 16:39:44 -0600

    downloaded all node modules and set up project

---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- Drops the employees if it exists currently --
+DROP DATABASE IF EXISTS employees_db;
+-- Creates the employees database --
+CREATE DATABASE employees_db;
+
+-- use employees database --
+USE employees_db;
