f6bbd67764807dff3bd8e39b5f090a009580c330
ecinematic <ecinematic@yahoo.com>
2023-04-17 09:50:51 -0500

    updated index.js to include addEmployee function and pseudocode the remaning

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { prompt } = require("inquirer");
+const db = require("./db");
 
+loadMainPrompts();
 
 function loadMainPrompts() {
     prompt([
@@ -66,7 +68,146 @@ function loadMainPrompts() {
         let choice = res.choice;
         console.log(choice);
         console.log(res);
+        switch (choice) {
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+                viewEmployeesByDepartment();
+                break;
+            case "VIEW_EMPLOYEES_BY_MANAGER":
+                viewEmployeesByManager();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "REMOVE_EMPLOYEE":
+                removeEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            case "UPDATE_EMPLOYEE_MANAGER":
+                updateEmployeeManager();
+                break;
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "REMOVE_DEPARTMENT":
+                removeDepartment();
+                break;
+            case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
+                viewUtilizedBudgetByDepartment();
+                break;
+            case "VIEW_ROLES":
+                viewRoles();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "REMOVE_ROLE":
+                removeRole();
+                break;
+            default:
+                quit();
+        }
     })
 }
 
-loadMainPrompts();
+
+// View Employees
+
+// View Employees Per Department
+
+// View Employees Per Manager
+
+// Add an Employee
+function addEmployee() {
+    prompt([
+      {
+        name: "first_name",
+        message: "What is the employee's first name?"
+      },
+      {
+        name: "last_name",
+        message: "What is the employee's last name?"
+      }
+    ])
+      .then(res => {
+        let firstName = res.first_name;
+        let lastName = res.last_name;
+  
+        db.findAllRoles()
+          .then(([rows]) => {
+            let roles = rows;
+            const roleChoices = roles.map(({ id, title }) => ({
+              name: title,
+              value: id
+            }));
+  
+            prompt({
+              type: "list",
+              name: "roleId",
+              message: "What is the employee's role?",
+              choices: roleChoices
+            })
+              .then(res => {
+                let roleId = res.roleId;
+  
+                db.findAllEmployees()
+                  .then(([rows]) => {
+                    let employees = rows;
+                    const managerChoices = employees.map(({ id, first_name, last_name }) => ({
+                      name: `${first_name} ${last_name}`,
+                      value: id
+                    }));
+  
+                    managerChoices.unshift({ name: "None", value: null });
+  
+                    prompt({
+                      type: "list",
+                      name: "managerId",
+                      message: "Who is the employee's manager?",
+                      choices: managerChoices
+                    })
+                      .then(res => {
+                        let employee = {
+                          manager_id: res.managerId,
+                          role_id: roleId,
+                          first_name: firstName,
+                          last_name: lastName
+                        }
+  
+                        db.createEmployee(employee);
+                      })
+                      .then(() => console.log(
+                        `Added ${firstName} ${lastName} to the database`
+                      ))
+                      .then(() => loadMainPrompts())
+                  })
+              })
+          })
+      })
+  }
+// Delete Employee
+
+// Update Employee Role
+
+// Update Employee Manager
+
+// View All Roles
+
+// Add a Role
+
+// Delete a Role
+
+// View All Departments
+
+// Add a Department
+
+// Delete a Department
+
+// Exit the application
